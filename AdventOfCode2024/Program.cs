5265a71b861e7a1157477f9962b587b18ed32066
Charlie <charlescwallace2017@gmail.com>
2024-12-08 20:05:45 -0500

    day 3 wip;

--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -1,1 +1,12 @@
 using static AdventOfCode2024.Solutions.Day2.Utilities;
+
+//1. Find all matching instances and add to list.
+
+
+//2. Split list into number pairs.
+
+
+//3. Multiply number pairs and add result to new list.
+
+
+//4. Sum number list for total.
---
+++ AdventOfCode2024/Solutions/Day3/Utilities.cs
@@ -0,0 +1,6 @@
+namespace AdventOfCode2024.Solutions;
+
+public static class Utilities
+{
+    
+}
